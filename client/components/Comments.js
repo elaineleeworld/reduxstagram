147624b77148157403c7a8420b7dfcebe6fa4807
Elaine Lee <elaneylee@gmail.com>
2018-12-31 19:13:38 -0800

    finished project

---
+++ client/components/Comments.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const Comments = React.createClass({
+	renderComment(comment, i){
+		console.log("COMMENT", comment);
+		return(
+			<div className="comment" key={i}>
+			<p>
+				<strong>{comment.user}</strong>
+				{comment.text}
+				<button className="remove-comment" onClick={this.props.removeComment.bind(null,this.props.params.postId, i)}>&times;</button>
+			</p>
+			</div>
+			)
+	},
+	handleSubmit(e){
+		//stop page refreshing
+		e.preventDefault();
+		console.log("submitting the form!");
+		console.log(this.refs)
+		const { postId } = this.props.params;
+		const author = this.refs.author.value;
+		const comment= this.refs.comment.value;
+		console.log("CONSOLE LOG ALL 3",postId, author, comment);
+		this.props.addComment(postId, author, comment);
+		this.refs.commentForm.reset();
+	},
+	render(){
+		return(
+		<div className='comment'>
+		{this.props.postComments.map(this.renderComment)}
+		<form ref='commentForm' className="comment-form" onSubmit={this.handleSubmit}>
+		<input type='text' ref='author' placeholder='author'/>
+		<input type='text' ref='comment' placeholder='comment'/>
+		<input type='submit' hidden/>
+		</form>
+		</div>
+		)
+	}
+});
+
+export default Comments;
